9d377fd2a9d25d2dc04d4b243db51ac098637fa4
PRAISE-C24 <104138599+PRAISE-C24@users.noreply.github.com>
2022-11-09 11:21:18 +0100

    edited and updated  the nav section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/navbar";
 import Home from "./Pages/Home/home";
 import About from "./Pages/About/about";
-import FindModel from "./Pages/FindModel/findmodel";
+import FindModel from "./Pages/FindModel/find-model";
 import JobPost from "./UI/Job-UI/JobPost/jobpost";
 import Contact from "./Pages/Contact/contact";
 import Community from "./UI/Admin-UI/CommunityPages/Community/community";
@@ -17,7 +17,7 @@ function App() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/jobpost" element={<JobPost />}></Route>
-        <Route path="/findmodel" element={<FindModel />}></Route>
+        <Route path="/find-model" element={<FindModel />}></Route>
         <Route path="/community" element={<Community />}></Route>
         <Route path="/magazine" element={<Magazine />}></Route>
       </Routes>
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,42 +1,107 @@
 import { Link, NavLink } from "react-router-dom";
 import { links } from "../../data";
-import { GoThreeBars } from "react-icons/go";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./navbar.css";
-import Logo from "../Logo/logo";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [deviceSize, setDeviceSize] = useState(window.innerWidth);
+
+  function handleMenu() {
+    setToggleMenu((prevToggle) => !prevToggle);
+  }
+
+  // setting device size
+  function handleResize() {
+    setDeviceSize(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [deviceSize]);
+
   return (
     <>
-      <nav>
+      <nav className="nav">
         <div className="container nav__container">
-          <Logo />
-          <div className="nav__menu">
-            <ul>
+          <Link to="/" className="logo">
+            <img src="./images/logo.png" alt="" />
+          </Link>
+
+          <div className="desktop-nav">
+            <ul className="desktop-nav__list">
               {links.map(({ name, path }, index) => {
-                return (
-                  <li>
-                    <NavLink
-                      to={path}
-                      className={({ isActive }) =>
-                        isActive ? "active-nav" : ""
-                      }
-                    >
-                      {name}
-                    </NavLink>
-                  </li>
-                );
+                //using device size to render specific num of items from the array
+                if (deviceSize < 920) {
+                  return (
+                    index <= 3 && (
+                      <li key={index}>
+                        <NavLink
+                          to={path}
+                          className={({ isActive }) => (isActive ? "active-nav" : "")}>
+                          {name}
+                        </NavLink>
+                      </li>
+                    )
+                  );
+                } else {
+                  return (
+                    <li key={index}>
+                      <NavLink
+                        to={path}
+                        className={({ isActive }) => (isActive ? "active-nav" : "")}>
+                        {name}
+                      </NavLink>
+                    </li>
+                  );
+                }
               })}
             </ul>
           </div>
 
-          <div className="nav__left">
+          <div className="desktop-nav__btn">
             <Link to="/" className="btn btn1">
               Login
             </Link>
             <button className="btn btn2">Signup</button>
-            <button className="nav__toggle-btn">
-              <GoThreeBars />
-            </button>
+          </div>
+
+          <FaBars
+            style={{ display: toggleMenu && "none" }}
+            onClick={handleMenu}
+            className="open-menu"
+          />
+        </div>
+      </nav>
+
+      {/* mobile nav */}
+
+      <nav style={{ transform: toggleMenu && `translateX(${0}%)` }} className="mobile-nav">
+        <div
+          style={{ transform: toggleMenu && `translateX(${0}%)` }}
+          className="mobile-nav__container">
+          <FaTimes onClick={handleMenu} className="close-menu" />
+          <ul className="mobile-nav__list">
+            {links.map(({ name, path }, index) => {
+              return (
+                <li onClick={handleMenu} key={index} className="mobile-nav__item">
+                  <NavLink to={path} className={({ isActive }) => (isActive ? "active-nav" : "")}>
+                    {name}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="mobile-nav__btn">
+            <Link to="/" className="btn btn1">
+              Login
+            </Link>
+            <button className="btn btn2">Sign-up</button>
           </div>
         </div>
       </nav>
