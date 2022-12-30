eadf71a075415cea1bbbcfc79ed4b89660ef728e
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-12-31 00:40:51 +0100

    Added model_card, image_card, previous_jobs, see_models components. They share their usage amongst the agency dashboard and profile.

---
+++ src/Components/SeeModels/see_models.jsx
@@ -0,0 +1,28 @@
+import ModelCard from "../ModelCard/model_card";
+import "./see_models.scss";
+
+const SeeModels = () => {
+  return (
+    <div id="see_models">
+      <div>
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+        <ModelCard scale="1" ch="380px" />
+      </div>
+    </div>
+  );
+};
+
+export default SeeModels;
