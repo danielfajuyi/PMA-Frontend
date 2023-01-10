9a49bece348c45351d56e743cd9a1f61ce8d15eb
presco <ubochi_precious@yahoo.com>
2023-01-10 20:48:20 +0100

    added client profile

---
+++ src/UI/Client/ClientProfile/About.jsx
@@ -0,0 +1,19 @@
+
+
+const About = () => {
+    return (
+      <div>
+          <div className='about'>
+              <h2>About Brand</h2>
+              <p>Fourwall Magazine is Nigeria's no. 1 model and pageant magazine</p> <br />
+              <p>we are focused on promoting and creating awareness for models</p>
+              </div>
+              <div className='industry'>
+              <h2>Industry</h2>
+              <p>Publishing</p>
+              </div>
+      </div>
+    )
+  }
+  
+  export default About
